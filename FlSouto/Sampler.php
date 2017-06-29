79381c643ec9464bd7b021a89f3ddf47343255e2
Fabio <ti@traca.com.br>
2017-06-29 13:06:29 -0300

    Refactor part feature

--- FlSouto/Sampler.php
+++ FlSouto/Sampler.php
@@ -178,6 +178,7 @@ class Sampler{
     }
     
     function part($offset, $length){
+        require_once(__DIR__.'/SamplerPart.php');
         $copy = $this->copy($offset,$length);
         $ref = new SamplerPart($copy->file, true);
         $ref->parent = $this;
@@ -214,31 +215,4 @@ class Sampler{
 
 }
 
-class SamplerPart extends Sampler{
 
-    /**
-     * @var Sampler
-     */
-    protected $parent;
-    protected $offset;
-    protected $length;
-    
-    function rebuild(){
-        $part1 = $this->parent->copy(0, $this->offset);
-        $part1->add($this);
-        $parent_len = $this->parent->len();
-        $offset2 = $this->offset + $this->length;
-        if($offset2 < $parent_len){
-            $part2 = $this->parent->copy($offset2, $parent_len-$offset2);
-            $part1->add($part2);
-        }
-        return $part1;
-    }
-    
-    function sync(){
-        $obj = $this->rebuild();
-        shell_exec("mv {$obj->file} {$this->parent->file}"); 
-        
-    }
-    
-}
---
+++ FlSouto/SamplerPart.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FlSouto;
+
+class SamplerPart extends Sampler{
+
+    /**
+     * @var Sampler
+     */
+    protected $parent;
+    protected $offset;
+    protected $length;
+
+    function rebuild(){
+        $part1 = $this->parent->copy(0, $this->offset);
+        $part1->add($this->cut(0,$this->length));
+        $parent_len = $this->parent->len();
+        $offset2 = $this->offset + $this->length;
+        if($offset2 < $parent_len){
+            $part2 = $this->parent->copy($offset2, $parent_len-$offset2);
+            $part1->add($part2);
+        }
+        return $part1;
+    }
+
+    function sync(){
+        $obj = $this->rebuild();
+        shell_exec("mv {$obj->file} {$this->parent->file}");
+
+    }
+
+}
